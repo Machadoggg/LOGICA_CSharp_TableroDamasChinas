dea540f68fdf2687fa7370c1b3b5871d9f442610
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-08-09 11:19:08 -0500

    Add project DamasChinas

---
+++ TableroDamas_evalart/TableroDamas_evalart/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program
+{
+    static int n = 5;
+
+    public static void Main()
+    {
+        if (n < 1 || n > 10)
+        {
+            n = 5;
+        }
+
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if ((i + j) % 2 == 0)
+                {
+                    Console.Write("X ");
+                }
+                else
+                {
+                    Console.Write("_ ");
+                }
+
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+}
+
